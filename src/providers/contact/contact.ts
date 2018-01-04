044066aa87a6fff25b6ec8f52794a21d587aa40c
Eduardo Morais <emorais1375@gmail.com>
2018-01-03 20:36:53 -0400

    Configurando Providers e pagina de editar contato

--- src/providers/contact/contact.ts
+++ src/providers/contact/contact.ts
@@ -1,11 +1,57 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
+import { DatePipe } from '@angular/common';
 
 @Injectable()
 export class ContactProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello ContactProvider Provider');
+  constructor(private storage: Storage, private datepipe: DatePipe) {    
   }
 
+  public insert(contact: Contact){
+    let key = this.datepipe.transform(new Date(), "ddMMyyyyHHmmss");
+    return this.save(key, contact);
+  }
+
+  public update(key: string, contact: Contact){
+    return this.save(key, contact);
+  }
+
+  private save(key: string, contact: Contact){
+    return this.storage.set(key, contact);
+  }
+
+  public remove(key: string){
+    return this.storage.remove(key);
+  }
+
+  public getAll(){
+    
+    let contacts: ContactList[] = [];
+
+    return this.storage.forEach((value: Contact, key: string, iterationNumber: number) => {
+      let contact = new ContactList();
+      contact.key = key;
+      contact.contact = value;
+      contacts.push(contact);
+    })
+    .then(() => {
+      return Promise.resolve(contacts);
+    })
+    .catch((error) => {
+      return Promise.reject(error);
+    });
+  }
+}
+
+export class Contact {
+  name: string;
+  phone: number;
+  birth: Date;
+  active: boolean;
+}
+
+export class ContactList {
+  key: string;
+  contact: Contact;
 }
